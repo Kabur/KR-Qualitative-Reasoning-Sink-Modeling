7604054c06e951da5b1c09812bfa3f950baeb7ed
antreashp <antreashp@gmail.com>
2017-10-16 18:50:44 +0300

    first commit test

--- Relationship.py
+++ Relationship.py
@@ -1,1 +1,10 @@
-class
+class Relationship:
+
+    def __init__(self,type,sign,source,target):
+        self.type=type
+        self.sign=sign
+        self.source=source
+        self.target=target
+
+
+
