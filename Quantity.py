5d55134dde2ffd67d3eb3373718024d19f137b2d
antreashp <antreashp@gmail.com>
2017-10-16 18:43:19 +0300

    first commit test

---
+++ Quantity.py
@@ -0,0 +1,8 @@
+class Quantity :
+    def __init__(self,value,derivative,range,exogenous):
+        self.value=value
+        self.derivative=derivative
+        self.range=range
+        self.exogenous=exogenous
+
+
